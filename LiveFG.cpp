d409fe2c33835dd462765dea578f8f457be31cef
Akshay Balakrishnan <akbgunner4ever@gmail.com>
2018-10-16 10:47:59 +0530

    Update LiveFG
    Indentation and small modifications. Thank you Alfin, very cool.

--- LiveFG.cpp
+++ LiveFG.cpp
@@ -17,10 +17,10 @@ void extract_frames()
 	try{
 
 		//open the video file
-  		VideoCapture cap("");// Add source here EXAMPLE: http://103.20.189.107:80/mjpg/video.mjpg?COUNTER // open the video file
-  		if(!cap.isOpened())  // check if we succeeded
+  		VideoCapture cap("");// Add source here; 
+  		if(!cap.isOpened())  // check if we succeeded;
   			CV_Error( -2 ,"Can not open Video file");
-	    int frameNumber=0,fc=100;
+	        int frameNumber=0,fc=100;
   		vector<int> compression_params;
   		compression_params.push_back(cv::IMWRITE_JPEG_QUALITY);
 		compression_params.push_back(100);
@@ -30,13 +30,13 @@ void extract_frames()
   		    {
   			   Mat frame;
   			   cap >> frame; 
-		       string filePath = outputDir + to_string(static_cast<long long>(frameNum))+ ".jpg";
+		           string filePath = outputDir + to_string(static_cast<long long>(frameNum))+ ".jpg";
 	  		   imwrite(filePath,frame,compression_params);
   		    }
   		}catch( cv::Exception& e )
   		{
-    		cerr << e.msg << endl;
-    		exit(1);
+    			cerr << e.msg << endl;
+    			exit(1);
   		}
 	
 }
